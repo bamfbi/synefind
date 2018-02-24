54a47a387b77901f3ab081735164ea1360654164
aawill <aawill@umich.edu>
2018-02-24 17:00:44 -0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import numpy as np
+import scipy
+from pyAudioAnalysis import audioAnalysis, analyzeMovieSound
+import matplotlib.pyplot as plt
+help(audioAnalysis)
 
-help(np)
+#[Fs, x] = audioBasicIO.readAudioFile("thunder_strike_3-Mike_Koenig-853886140.wav");
+#F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+#plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR'); 
+#plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy'); plt.show()
+
