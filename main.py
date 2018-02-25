83b97c7f43821522a088754f6967bd2e54360c0a
luclepot <luclepot@umich.edu>
2018-02-25 00:40:41 +0000

    committing

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import numpy as np
 import scipy as sp 
-from pyAudioAnalysis import audioAnalysis, audioBasicIO
+from pyAudioAnalysis import audioBasicIO
 import matplotlib.pyplot as plt
 
-def sinewave(frequency, amplitude, sampleRate=44100, path="data/outfile.wav", length=1):
+def sinewave_write(frequency=440., amplitude=20000., sampleRate=44100, path="data/outfile.wav", length=1):
     sine = np.zeros(length*sampleRate)
+    print np.size(sine)
     for i in range(np.size(sine)):
-        sine[i] = amplitude*np.sin(frequency*2*np.pi*(i/sampleRate))
-    sp.io.wavfile.write(path, sampleRate, sine)
+        sine[i] = amplitude*np.sin(frequency*2.*np.pi*(float(i)/float(sampleRate)))
+    sp.io.wavfile.write(path, sampleRate, np.int16(sine))
+    
 
-sinewave(440, )
+
+sinewave(amplitude = 30000, length = 12)
         
+a, b = audioBasicIO.readAudioFile("data/outfile.wav")
 
-a, b = audioBasicIO.readAudioFile("data/wav3.WAV")
-print "sample rate: ", a
-b = np.mean(b, axis=1)
-
-plt.plot(range(np.size(b)), b)
-help(ps)
-plt.show()
-
-#[Fs, x] = audioBasicIO.readAudioFile("thunder_strike_3-Mike_Koenig-853886140.wav");
-#F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
-#plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR'); 
-#plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy'); plt.show()
-
+#plt.plot(range(np.size(b)), b)
+#plt.show()
